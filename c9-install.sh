622b3ee5ae9564624e40d825c25c7cdfa96a96db
lsentkiewicz <sirmims@gmail.com>
2016-07-03 18:09:30 +0200

    Update c9-install.sh

--- c9-install.sh
+++ c9-install.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+exec 2>&1
+dir=$(pwd)
+cd /tmp
+exec > >(tee -i /temp/dump.log) 
+cd $dir
+
 . ~/.nvm/nvm.sh
 nvm install v6
 nvm alias default node
 npm i -g restcoder-cli
-The CLI has been installed. Type `restcoder login` to log in.
+echo 'The CLI has been installed. Type `restcoder login` to log in.'
