8687dc8797fe4f0cdb6cd15be600dfac052002a8
lsentkiewicz <sirmims@gmail.com>
2016-07-04 10:00:55 +0200

    Update c9-install-ruby.sh

--- c9-install-ruby.sh
+++ c9-install-ruby.sh
@@ -3,6 +3,7 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 rvm install 2.3.1
+rvm --default use 2.3.1
 gem install bundler
 ruby -v
 bundler -v
