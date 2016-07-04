4bd9c71b93913d48233cc74c9250b5daa304b845
lsentkiewicz <sirmims@gmail.com>
2016-07-04 19:20:53 +0200

    Update c9-install-python.sh

--- c9-install-python.sh
+++ c9-install-python.sh
@@ -2,7 +2,7 @@
 
 curl -s https://bootstrap.pypa.io/get-pip.py | python
 sudo pip install --upgrade pip
-pip install virtualenv
+sudo pip install virtualenv
 python --version
 pip --version
 echo -e '\e[92mpython and pip installed successfully'
